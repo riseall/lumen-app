c6597bb5c82a5604c2738a395a42f4d0cb685b93
Rizal Nugroho <rizalnug2@gmail.com>
2024-03-26 02:30:33 +0700

    Requesting an Individual Book

--- tests/app/Http/Controllers/BooksControllerTest.php
+++ tests/app/Http/Controllers/BooksControllerTest.php
@@ -14,19 +14,37 @@ class BooksControllerTest extends TestCase
      *
      * @return void
      */
-    public function test_that_base_endpoint_returns_a_successful_response()
-    {
-        $this
-            ->get('/books')
-            ->seeJson([
-                'title' => 'Wayahe Ngopi'
-            ])
-            ->seeJson([
-                'title'=> '21 Lessons'
-            ]);
+    //public function test_that_base_endpoint_returns_a_successful_response()
+    //{
+        // $this
+        //     ->get('/books')
+        //     ->seeJson([
+        //         'title' => 'Wayahe Ngopi'
+        //     ])
+        //     ->seeJson([
+        //         'title' => '21 Lessons'
+        //     ]);
 
-    /*$this->assertEquals(
+        /*$this->assertEquals(
             $this->app->version(), $this->response->getContent()
         );*/
+    //}
+
+    /** @test **/
+    public function show_should_return_a_valid_book()
+    {
+        $this->markTestIncomplete('Pending test');
+    }
+
+    /** @test **/
+    public function show_should_fail_when_the_book_id_does_not_exist()
+    {
+        $this->markTestIncomplete('Pending test');
+    }
+
+    /** @test **/
+    public function show_route_should_not_match_an_invalid_route()
+    {
+        $this->markTestIncomplete('Pending test');
     }
 }
