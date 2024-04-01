ed0fdaa1f18aa344761456f1e79c1544ed1d5864
Rizal Nugroho <rizalnug2@gmail.com>
2024-04-01 08:48:00 +0700

    controller

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Book;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
 
 /**
  * Class BooksController
@@ -39,5 +40,46 @@ class BooksController extends Controller
             ], 404);
         }
     }
-    //
+
+    /**
+     * POST /books
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function store(Request $request)
+    {
+        $book = Book::create($request->all());
+
+        return response()->json(
+            ['created' => true],
+            201,
+            ['Location' => route('books.show', ['id' => $book->id])]
+        );
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $book = Book::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'error' => [
+                    'message' => 'Book not found'
+                ]
+            ], 404);
+        }
+
+        $book->fill($request->all());
+        $book->save();
+
+        return $book;
+    }
+
+    public function destroy($id)
+    {
+        $book = Book::findOrFail($id);
+        $book->delete();
+
+        return response(null, 204);
+    }
 }
