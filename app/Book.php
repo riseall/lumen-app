eb3ca55797e6f5fafd711ba890f0d0a2d19d1672
Rizal Nugroho <rizalnug2@gmail.com>
2024-04-01 08:47:12 +0700

    model

--- app/Book.php
+++ app/Book.php
@@ -1,7 +1,15 @@
 <?php
-    namespace App;
-    use Illuminate\Database\Eloquent\Model;
 
-    class Book extends Model{
+namespace App;
 
-    }
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    /**
+     * The attributes that are mass assignable
+     *
+     * @var array
+     */
+    protected $fillable = ['title', 'description', 'author'];
+}
